10b540c736df2256737762cb7875fa960b706fc1
Jörg Hohwiller <hohwille@users.sourceforge.net>
2020-09-16 22:23:36 +0200

    added NPE demo

--- src/main/java/com/devonfw/java/newfeatures/java14/Java14HelpfulNullPointerExceptions.java
+++ src/main/java/com/devonfw/java/newfeatures/java14/Java14HelpfulNullPointerExceptions.java
@@ -1,8 +1,20 @@
 package com.devonfw.java.newfeatures.java14;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
 /**
- * Shows <a href="https://openjdk.java.net/jeps/358">JEP 358</a>: Helpful NullPointerExceptions
+ * Shows <a href="https://openjdk.java.net/jeps/358">JEP 358</a>: Helpful NullPointerExceptions<br>
+ * You need to run this with JVM option "-XX:+ShowCodeDetailsInExceptionMessages"
  */
 public class Java14HelpfulNullPointerExceptions {
+
+  public static void main(String[] args) {
+
+    List<Supplier<String>> list = new ArrayList<>();
+    list.add(() -> null);
+    System.out.println(list.get(0).get().length());
+  }
 
 }
